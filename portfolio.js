3666eb2b65f32195901c9b7a6f8ce8963f61fcc4
Amf1b1an <fabiansandberg0@gmail.com>
2025-06-08 13:44:46 +0200

    final

--- portfolio.js
+++ portfolio.js
@@ -2,21 +2,21 @@ const projects = [
   {
     title: "Game Hub",
     description: "This is the first project i made at Noroff. This website is my contribution for Noroffs cross-course project. Which means it has been part of several courses during the first year, starting with the design course and HTML & CSS course in the first semester, and the JavaScript course in the second semester.",
-    thumbnail: "images/GameHub_Logo.png",
+    thumbnail: "images/JS-CA.png",
     liveLink: "https://amf1b1an.github.io/JS-CA/",
     repoLink: "https://github.com/Amf1b1an/JS-CA"
   },
   {
     title: "Semester Project 1",
-    description: "In this semester project we were tasked with creating a website using what we've learnt about design, html and css during the first semester. ",
-    thumbnail: "images/Semester_project_Logo.png",
+    description: "In this semester project we were tasked with creating a website using what we've learnt about design, html and css during the first semester. The challange in this project was to make things work without javascript",
+    thumbnail: "images/SP1.png",
     liveLink: "https://amf1b1an.github.io/Semester-Project-1/",
     repoLink: "https://github.com/Amf1b1an/Semester-Project-1"
   },
   {
     title: "Exam Project 1",
-    description: "...",
-    thumbnail: "..",
+    description: "This is my first exam project at Noroff. The assignment was to create a blog website, and i decided to create a blog website about music. This project were made from scratch with figma for the design part, and html, css and javascript.",
+    thumbnail: "images/PE1.png",
     liveLink: "https://68336d3d0b969f3b4387813b--lustrous-starlight-0460ae.netlify.app/",
     repoLink: "https://github.com/Amf1b1an/FED1-PE1-Amf1b1an1"
   }
